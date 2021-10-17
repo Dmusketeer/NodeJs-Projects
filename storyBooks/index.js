7aaa9bcdc252e1be771595d03bf1fc6037a811ac
Dmusketeer <ardmountronics@gmail.com>
2021-10-18 00:02:24 +0530

    connection to mongodb

--- storyBooks/index.js
+++ storyBooks/index.js
@@ -4,6 +4,23 @@ const mongoose = require('mongoose')
 
 const app = express();
 
+const Db_password = process.env.MONGO_PASSWORD
+const MONGO_URI = `mongodb+srv://dheeraj:${Db_password}@atlastesting.1itdz.mongodb.net/storyBook?retryWrites=true&w=majority`;
+
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+mongoose.Promise = global.Promise;
+mongoose.connection.once('open', () => {
+    console.log('DataBase Connected ........')
+})
+
+mongoose.connection.on('error', (err) => {
+    console.log('Connection Error ........')
+})
+
 app.set('view engine', 'ejs');
 
 const port = process.env.PORT || 3000;
